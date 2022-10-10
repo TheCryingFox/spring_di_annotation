88a6d8a446f065dc612b31dfea5460f570082c77
TheCryingFox <2714430336@qq.com>
2022-10-10 16:50:12 +0800

    Initial commit

---
+++ src/main/java/org/xiaomi/dao/UserDao.java
@@ -0,0 +1,15 @@
+package org.xiaomi.dao;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.xiaomi.entity.Users;
+//@Component
+@Repository("userDao999")
+public class UserDao implements  UserDaoIfac{
+
+    public Users selectUserById(Integer id) {
+        System.out.println("get User,userid:"+id);
+        return new Users();
+    }
+
+}
---
+++ src/main/java/org/xiaomi/service/TestUserService.java
@@ -0,0 +1,25 @@
+package org.xiaomi.service;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.xiaomi.dao.OrderDaoIfac;
+
+public class TestUserService {
+
+    public static void main(String[] args) {
+
+        ApplicationContext act=new ClassPathXmlApplicationContext("beans.xml");
+
+        UserServiceIfac userService1=(UserServiceIfac) act.getBean("userService");
+//        UserServiceIfac userService2=(UserServiceIfac) act.getBean("userService");
+//        UserServiceIfac userService3=(UserServiceIfac) act.getBean("userService");
+//
+        System.out.println(userService1.login(1));
+//        System.out.println(userService2==userService3);
+
+//        OrderDaoIfac orderDaoIfac=(OrderDaoIfac) act.getBean(org.xiaomi.dao.OrderDao.class);
+//
+//        System.out.println(orderDaoIfac.selectAllOrder(1));;
+
+    }
+}
